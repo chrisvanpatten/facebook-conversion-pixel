b472b9c262e1d0c074f75030bf56ebe80a8d49a1
Kellen Mace <kellen.mace@gmail.com>
2014-06-11 18:25:26 -0400

    initial commit

---
+++ facebook-conversion-pixel.php
@@ -0,0 +1,109 @@
+<?php
+/*
+Plugin Name: Facebook Conversion Pixel
+Plugin URI: n/a
+Description: Add Facebook Conversion Pixels to Posts, Pages, or any other custom post types.
+Version: 1.0
+Author: Kellen Mace
+Author URI: http://kellenmace.com/
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Based on Vincent Astolfi's (http://www.nueue.net/) "Facebook Pixel Conversions for WordPress" plugin
+*/
+
+/**
+ * Insert Facebook Conversion Pixel
+ * @since  0.1.0
+ */
+if ( ! is_admin() ) {
+	function fb_pxl_head() {
+		 global $post;
+		 $the_id = $post->ID;
+		 $fb_pxl_options = get_option( 'fb_pxl_options' );
+
+		 // If user has not disabled this post type
+		 if ( 'on' != $fb_pxl_options[ $post->post_type ] ) {
+		 	$fb_pxl_switch = get_post_meta($the_id, 'fb_pxl_checkbox', true);
+
+		 	// If user has chosen to insert code, insert it
+			if ( 'on' == $fb_pxl_switch ) {
+			 	$fb_pxl_code = get_post_meta($the_id, 'fb_pxl_conversion_code', true);
+			 	echo $fb_pxl_code;
+			 }
+		 }
+	}
+	add_action( 'wp_head', 'fb_pxl_head' );
+}
+
+/**
+ * Display meta box in admin
+ * @since  0.1.0
+ */
+if ( is_admin() ) {
+	function fb_pxl_meta( array $meta_boxes ) {
+		$prefix = 'fb_pxl_';
+		$options = get_option( 'fb_pxl_options' );
+		$pages = array();
+		foreach ( $options as $option_key => $option_value ) {
+			if ( 'on' != $option_value )
+				array_push( $pages, $option_key );
+		}
+
+		$meta_boxes[] = array(
+			'id'         => 'fb_pxl_metabox',
+			'title'      => 'Facebook Pixel Conversion Code',
+			'pages'      => $pages,
+			'context'    => 'normal',
+			'priority'   => 'high',
+			'show_names' => true, // Show field names on the left
+			'fields'     => array(     
+				array(
+					'name' => 'Insert Code',
+					'desc' => 'Insert Facebook Conversion Pixel code',
+					'id'   => $prefix . 'checkbox',
+					'type' => 'checkbox',
+				),
+				array(
+					'name' => 'Conversion Pixel JavaScript',
+					'desc' => 'Paste your Facebook Conversion Pixel code here',
+					'id'   => $prefix . 'conversion_code',
+					'type' => 'textarea_code',
+				),
+			),
+		);
+		return $meta_boxes;
+	}
+	add_filter( 'cmb_meta_boxes', 'fb_pxl_meta' );
+
+	/**
+	 * Include Custom Metaboxes and Fields Library
+	 * @since  0.1.0
+	 */
+	function fb_pxl_init_mtbxs() {
+		if ( ! class_exists( 'cmb_Meta_Box' ) )
+			require_once( plugin_dir_path( __FILE__ ) . 'includes/init.php' );
+	}
+	add_action( 'init', 'fb_pxl_init_mtbxs', 9999 );
+	
+	/**
+	 * Display settings link on WP plugin page
+	 * @since  0.1.0
+	 */
+	function fb_pxl_plugin_action_links( $links, $file ) {
+		$plugin_file = 'facebook-conversion-pixel/facebook-conversion-pixel.php';
+		if ( $file == $plugin_file ) {
+			$settings_link = '<a href="' . admin_url( 'admin.php?page=fb_pxl_options' ) . '">' . __( 'Settings', 'facebook-conversion-pixel' ) . '</a>';
+			array_unshift( $links, $settings_link );
+		}
+		return $links;
+	}
+	add_filter( "plugin_action_links", 'fb_pxl_plugin_action_links', 10, 4 );
+
+	/**
+	 * Include plugin options page
+	 * @since  0.1.0
+	 */
+	include_once( plugin_dir_path( __FILE__ ) . 'includes/admin.php' );
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,44 @@
+=== Facebook Conversion Pixel ===
+Contributors: kellenmace, vincentastolfi
+Donate link: n/a
+Tags: facebook, conversion, pixel, facebook pixel, facebook conversion, conversion pixel, code, add, insert
+Requires at least: 3.0
+Tested up to: 3.9
+Stable tag: 0.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+ == Description ==
+Insert Facebook Conversion Pixel code into to Posts, Pages, or any other post types
+
+ == Installation ==
+1. Download the plugin
+2. Upload to "/wp-content/plugins" folder or upload through WordPress interface
+3. Activate the Plugin
+
+== Frequently Asked Questions ==
+
+= How do I get a Conversion Tracking Pixel code? =
+
+Click <a href="https://www.facebook.com/ads/manage">Here</a> and make sure you are logged into Facebook. Then on the left-hand side, click "Power Editor," and in the top right you'll see a button that reads Create Conversion Pixel. Copy the JavaScript code that is displayed.
+
+= How do I insert the Facebook Conversion Pixel code on a certain page or post? =
+
+In the WordPress Admin Dashboard, begin editing the Post, Page, or other post type that you want to change. Toward the bottom of the editor page, you'll see a "Facebook Pixel Conversion Code" box. Paste your Facebook Conversion Pixel JavaScript code into the box, make sure the "Insert Code" checkbox is checked, then save the post/page. Your code will now be inserted into that post/page every time it is loaded.
+
+= How do I hide the Facebook Conversion Pixel field on certain post types? =
+
+From the WordPress Admin Dashboard, go to Settings > Facebook Conversion Pixel. Check the boxes next to Page, Post, or any other post type for which you'd like to disable the plugin.
+
+== Changelog ==
+
+= 0.1 =
+* Initial release
+
+== Screenshots ==
+1. Paste in your Facebook Conversion Pixel code and check the Insert Code checkbox to insert it.
+
+2. Disable the plugin on Page, Post, or any other post type if desired on the plugin options page.
+
+== Notes ==
+Based on Vincent Astolfi's (http://www.nueue.net/) "Facebook Pixel Conversions for WordPress" plugin.
